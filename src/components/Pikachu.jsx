92af5ddbb39ab991725101fce6b04b8dbfaa2ba3
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-24 16:44:58 -0700

    Move useEffect into PokemonCard

--- src/components/Pikachu.jsx
+++ src/components/Pikachu.jsx
@@ -1,14 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PokemonCard from "./PokemonCard";
 
 export default function Pikachu() {
-	const [img, setImg] = useState(" ");
-
-	useEffect(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon/pikachu", { mode: "cors" })
-			.then((res) => res.json())
-			.then((res) => setImg(res.sprites.front_default));
-	}, []);
-
-	return <PokemonCard pokemon={"pikachu"} color={"yellow"} img={img} />;
+	return <PokemonCard pokemon={"pikachu"} color={"yellow"} />;
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function PokemonCard({ pokemon, color, img }) {
+export default function PokemonCard({ pokemon, color }) {
+	const [imgSrc, setImgSrc] = useState(" ");
+
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, { mode: "cors" })
+			.then((res) => res.json())
+			.then((res) => setImgSrc(res.sprites.front_default));
+	}, [pokemon]);
+
 	return (
 		<div style={{ backgroundColor: color }}>
-			<img src={img} alt={pokemon} />
+			<img src={imgSrc} alt={pokemon} />
 			<h1>{pokemon}</h1>
 		</div>
 	);
