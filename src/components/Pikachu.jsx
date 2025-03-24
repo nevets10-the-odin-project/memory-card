a7765de242799fb64ffc4490161c73adde0fee76
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-24 16:47:05 -0700

    Change background color for Pikachu

--- src/components/Pikachu.jsx
+++ src/components/Pikachu.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 
 export default function Pikachu() {
-	return <PokemonCard pokemon={"pikachu"} color={"yellow"} />;
+	return <PokemonCard pokemon={"pikachu"} color={"rgb(250, 192, 0)"} />;
 }
