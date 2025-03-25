325c62de6df6442ce38c1dafda2d054b7f87e590
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-24 17:52:20 -0700

    Add shuffle function to Deck

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import { pokemon } from "../data";
 import PokemonCard from "./PokemonCard";
 
+function shuffle(array) {
+	// Fisher–Yates shuffle
+	var m = array.length,
+		t,
+		i;
+
+	while (m) {
+		i = Math.floor(Math.random() * m--);
+
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+	}
+
+	return array;
+}
+
 export default function Deck() {
 	return (
 		<div className="deck">
-			{pokemon.map((p) => (
+			{shuffle(pokemon).map((p) => (
 				<PokemonCard id={p.id} pokemon={p.name} type={p.type} />
 			))}
 		</div>
