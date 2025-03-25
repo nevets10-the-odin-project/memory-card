6e1e137601df92b769501b40332fcc679d18a0da
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-24 17:06:42 -0700

    Add Squirtle, add styles to Squirtle and App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import Pikachu from "./components/Pikachu";
+import Squirtle from "./components/Squirtle";
 
 function App() {
 	const [score, setScore] = useState(0);
 	const [highScore, setHighScore] = useState(0);
 
 	return (
-		<>
+		<div className="deck">
 			<Pikachu />
-		</>
+			<Squirtle />
+		</div>
 	);
 }
 
---
+++ src/components/Squirtle.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import PokemonCard from "./PokemonCard";
+
+export default function Squirtle() {
+	return <PokemonCard pokemon={"squirtle"} type={"water"} />;
+}
